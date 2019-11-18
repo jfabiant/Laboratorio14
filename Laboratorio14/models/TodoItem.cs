790c3d7e2602779cbac86fcdb0013bc457351934
jhovin <jhovin.bonifacio@tecsup.edu.pe>
2019-11-18 15:38:03 -0500

    first commit

---
+++ Laboratorio14/models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio14.models
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
